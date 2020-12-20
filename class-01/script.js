08887e8379cfe1eb35903123f17068e9d8083533
naveed-rana <rana.naveed812@gmail.com>
2020-12-20 09:18:10 +0500

    class 1

---
+++ class-01/script.js
@@ -0,0 +1,25 @@
+var num = 5;
+num = num + 1;
+console.log("num variable" , num);
+// num++  === num + 1
+// alertnative of num + 1;
+// Prefix first add the value into variable than assign to new variable 
+var b = 5;
+var result = ++b;
+console.log("result variable for b", result)
+// Postfix first assign the var value to new variable than add into 1 into variable
+var c = 5;
+var output = c++; 
+console.log("var output for variable c", output);
+
+// print c
+console.log("print c var", c);
+
+// num-- > num - 1
+// prefix or postfix
+
+
+// alert(c);
+
+var calculateLongStatement = 2 + 3 / 4 + 1 * ( 3 - 2 ) ;
+console.log("calculateLongStatement", calculateLongStatement);
